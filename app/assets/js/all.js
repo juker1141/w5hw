9102a810e7718b7a68442f0d7ca754d609742e14
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-05-20 00:11:38 +0800

    update admin.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,16 +1,15 @@
-$(".search_input").hide();
-$(".search").click(function (e) { 
+$('.search_input').hide();
+$('.search').click(function (e) {
   e.preventDefault();
-  $(".search_input").toggle();
+  $('.search_input').toggle();
 });
 
-
-$(".card_reply").hide();
-$(".btn_reply-show").click(function (e) { 
+$('.card_reply').hide();
+$('.btn_reply-show').click(function (e) {
   e.preventDefault();
-  $(".card_reply").show(400);
+  $('.card_reply').show(400);
 });
-$(".btn_reply-hide").click(function (e) { 
+$('.btn_reply-hide').click(function (e) {
   e.preventDefault();
-  $(".card_reply").hide(400);
+  $('.card_reply').hide(400);
 });
