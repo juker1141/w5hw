dd36a2b3560fa2f058d541d47bfd9af8414532fa
juker <juker1141@gmail.com>
2020-05-19 17:00:02 +0800

    update admin.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,16 @@
+$(".search_input").hide();
+$(".search").click(function (e) { 
+  e.preventDefault();
+  $(".search_input").toggle();
+});
+
+
 $(".card_reply").hide();
 $(".btn_reply-show").click(function (e) { 
   e.preventDefault();
   $(".card_reply").show(400);
+});
+$(".btn_reply-hide").click(function (e) { 
+  e.preventDefault();
+  $(".card_reply").hide(400);
 });
