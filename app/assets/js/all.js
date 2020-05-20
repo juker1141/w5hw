8c60577655450f2d806bdc4ae04aedde741c782f
juker1141 <50107434+juker1141@users.noreply.github.com>
2020-05-20 22:36:13 +0800

    update admin.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,3 +13,11 @@ $('.btn_reply-hide').click(function (e) {
   e.preventDefault();
   $('.card_reply').hide(400);
 });
+
+
+$('.admin_information_footer').hide();
+$('.adminOptions_btn').click(function (e) {
+  e.preventDefault();
+  $('.admin_information_footer').slideToggle(100);
+  $('.adminOptions_btn').toggleClass('active')
+});
